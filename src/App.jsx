d0ac8bbdb27172c9a2d81f55902b09f579973b6f
dan-ruiz <danielruiz2009@hotmail.com>
2024-08-15 18:16:16 -0500

    Creacion proyecto parcial FE3

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Card from "./components/Card";
+
+function App() {
+  return (
+    <>
+      <h1>Carga de estudiantes</h1>
+      <form></form>
+      <Card />
+    </>
+  );
+}
+
+export default App;
